6dfe80f4a279d400ae8d50f84edfa1df2a579328
Philip Burggraf <philip@pburggraf.de>
2016-08-03 17:01:51 +0200

    # better detector handling

--- src/PB/LicensePlate/LicensePlateFactory.php
+++ src/PB/LicensePlate/LicensePlateFactory.php
@@ -3,6 +3,7 @@
 namespace PB\LicensePlate;
 
 use PB\LicensePlate\Detector\AbstractDetector;
+use PB\LicensePlate\Response\LicensePlateResponse;
 
 /**
  * @author Philip Burggraf <philip@pburggraf.de>
@@ -10,12 +11,30 @@ use PB\LicensePlate\Detector\AbstractDetector;
 class LicensePlateFactory
 {
     /**
+     * @var string
+     */
+    protected $licensePlate;
+
+    /**
+     * @param string $licensePlate
+     */
+    public function __construct($licensePlate)
+    {
+        $this->licensePlate = $licensePlate;
+    }
+
+    /**
      * @param string $licensePlate
      * @param string $detectorType
-     * @return
+     *
+     * @return LicensePlateResponse
      */
     public static function fromString($licensePlate, $detectorType)
     {
+        if (!class_exists($detectorType)) {
+            throw new \RuntimeException('Detector not found');
+        }
+
         /** @var AbstractDetector $detector */
         $detector = new $detectorType($licensePlate);
 
