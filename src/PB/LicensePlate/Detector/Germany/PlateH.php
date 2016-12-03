3404ce8b004c8d10eb43793163441aef6709189b
Philip Burggraf <philip@pburggraf.de>
2016-12-03 14:12:08 +0100

    # completed specific tests starting with h and fixed a bug

--- src/PB/LicensePlate/Detector/Germany/PlateH.php
+++ src/PB/LicensePlate/Detector/Germany/PlateH.php
@@ -15,16 +15,16 @@ class PlateH extends AbstractGermanyPlate
     protected static $name = [
         'Stadt Hannover',
         'Region Hannover',
-        'Zentrale Polizeidirektion Niedersachsen',
         'Polizeidirektion Hannover',
+        'Zentrale Polizeidirektion Niedersachsen',
     ];
 
     /**
      * @var string[]
      */
     protected static $regexes = [
-        '/^H (?:(?:[AC-EH-Z][A-CE-Z]|[A-OQ-Y]D) [1-9][0-9]{2}|[BFG][A-Z] [1-9][0-9]{3})$/',
-        '/^H (?:[AC-EH-Z][A-CE-Z] (?:[1-9][0-9]|[1-9][0-9]{3})|[BFG][A-Z] [1-9][0-9]{1,2}|[ZP]D [1-9][0-9])$/',
+        '/^H (?:(?:[AC-EH-Z][A-CE-Z]|[A-OQ-Y]D) [1-9][0-9]{2}|[BFG][A-Z] [1-9][0-9]{2,3})$/',
+        '/^H (?:[AC-EH-Z][A-CE-Z] (?:[1-9][0-9]{0,1}|[1-9][0-9]{3})|[BFG][A-Z] [1-9][0-9]{0,1}|[ZP]D [1-9][0-9]{0,1}|[A-Z] [1-9][0-9]{0,3})$/',
         '/^H PD [1-9][0-9]{2,3}$/',
         '/^H ZD [1-9][0-9]{2,3}$/',
     ];
---
+++ tests/PB/LicensePlate/Detector/Germany/PlateHoTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace PB\LicensePlate\Detector\Germany;
+
+use PB\LicensePlate\Detector\GermanyDetector;
+use PB\LicensePlate\LicensePlateFactory;
+use PB\LicensePlate\Test\Detector\Germany\AbstractGermanyPlate;
+
+/**
+ * @author Philip Burggraf <philip@pburggraf.de>
+ */
+class PlateHbTest extends AbstractGermanyPlate
+{
+    /**
+     * @var array
+     */
+    protected static $plateTests = [
+        [
+            'plate' => 'HO A 1',
+            'description' => [
+                'Stadt Hof'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'HO A 10',
+            'description' => [
+                'Stadt Hof'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'HO A 100',
+            'description' => [
+                'Stadt Hof'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'HO A 1000',
+            'description' => [
+                'Stadt Hof'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'HO AA 1',
+            'description' => [
+                'Stadt Hof'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'HO AA 10',
+            'description' => [
+                'Stadt Hof'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'HO AA 100',
+            'description' => [
+                'Landkreis Hof'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'HO AA 1000',
+            'description' => [
+                'Landkreis Hof'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+    ];
+
+    /**
+     * @dataProvider getDataProvider
+     *
+     * @param $plate
+     * @param $description
+     * @param $type
+     */
+    public function testDescription($plate, $description, $type)
+    {
+        return parent::testDescription($plate, $description, $type);
+    }
+
+    /**
+     * @dataProvider getDataProvider
+     *
+     * @param $plate
+     * @param $description
+     * @param $type
+     */
+    public function testType($plate, $description, $type)
+    {
+        return parent::testDescription($plate, $description, $type);
+    }
+}
