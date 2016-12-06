c98cc31c56524d35b0596f90840468e4be02c003
Philip Burggraf <philip@pburggraf.de>
2016-12-06 22:05:54 +0100

    # completed specific tests starting with l and fixed a bug

---
+++ tests/PB/LicensePlate/Detector/Germany/PlateLTest.php
@@ -0,0 +1,268 @@
+<?php
+
+namespace PB\LicensePlate\Detector\Germany;
+
+use PB\LicensePlate\Detector\GermanyDetector;
+use PB\LicensePlate\LicensePlateFactory;
+use PB\LicensePlate\Test\Detector\Germany\AbstractGermanyPlate;
+
+/**
+ * @author Philip Burggraf <philip@pburggraf.de>
+ */
+class PlateLTest extends AbstractGermanyPlate
+{
+    /**
+     * @var array
+     */
+    protected static $plateTests = [
+        [
+            'plate' => 'L A 1',
+            'description' => [
+                'Stadt Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'L A 10',
+            'description' => [
+                'Stadt Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'L A 100',
+            'description' => [
+                'Stadt Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'L A 1000',
+            'description' => [
+                'Stadt Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'L T 1',
+            'description' => [
+                'Stadt Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'L T 10',
+            'description' => [
+                'Stadt Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'L T 100',
+            'description' => [
+                'Stadt Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'L T 1000',
+            'description' => [
+                'Stadt Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'L U 1',
+            'description' => [
+                'Landkreis Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'L U 10',
+            'description' => [
+                'Landkreis Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'L U 100',
+            'description' => [
+                'Landkreis Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'L U 1000',
+            'description' => [
+                'Landkreis Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'L Z 1',
+            'description' => [
+                'Landkreis Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'L Z 10',
+            'description' => [
+                'Landkreis Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'L Z 100',
+            'description' => [
+                'Landkreis Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'L Z 1000',
+            'description' => [
+                'Landkreis Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+
+        [
+            'plate' => 'L AA 1',
+            'description' => [
+                'Stadt Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'L AA 10',
+            'description' => [
+                'Stadt Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'L AA 100',
+            'description' => [
+                'Stadt Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'L AA 1000',
+            'description' => [
+                'Stadt Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'L TZ 1',
+            'description' => [
+                'Stadt Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'L TZ 10',
+            'description' => [
+                'Stadt Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'L TZ 100',
+            'description' => [
+                'Stadt Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'L TZ 1000',
+            'description' => [
+                'Stadt Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'L UA 1',
+            'description' => [
+                'Landkreis Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'L UA 10',
+            'description' => [
+                'Landkreis Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'L UA 100',
+            'description' => [
+                'Landkreis Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'L UA 1000',
+            'description' => [
+                'Landkreis Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'L ZZ 1',
+            'description' => [
+                'Landkreis Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'L ZZ 10',
+            'description' => [
+                'Landkreis Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'L ZZ 100',
+            'description' => [
+                'Landkreis Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'L ZZ 1000',
+            'description' => [
+                'Landkreis Leipzig'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+    ];
+
+    /**
+     * @dataProvider getDataProvider
+     *
+     * @param $plate
+     * @param $description
+     * @param $type
+     */
+    public function testDescription($plate, $description, $type)
+    {
+        return parent::testDescription($plate, $description, $type);
+    }
+
+    /**
+     * @dataProvider getDataProvider
+     *
+     * @param $plate
+     * @param $description
+     * @param $type
+     */
+    public function testType($plate, $description, $type)
+    {
+        return parent::testDescription($plate, $description, $type);
+    }
+}
