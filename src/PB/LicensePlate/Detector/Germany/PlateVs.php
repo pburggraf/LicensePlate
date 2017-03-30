cc15a303d15fd9aa3d6ad341976b0371650ea3ee
Philip Burggraf <philip@pburggraf.de>
2017-03-30 18:05:25 +0200

    + german plates starting with v

---
+++ src/PB/LicensePlate/Detector/Germany/PlateVs.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace PB\LicensePlate\Detector\Germany;
+
+use PB\LicensePlate\Detector\GermanyDetector;
+
+/**
+ * @author Philip Burggraf <philip@pburggraf.de>
+ */
+class PlateVs extends AbstractGermanyPlate
+{
+    /**
+     * @var string[]
+     */
+    protected static $name = [
+        'Schwarzwald-Baar-Kreis',
+    ];
+}
