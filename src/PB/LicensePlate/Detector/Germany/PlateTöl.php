f9d2f8ca7b4dde7c1c8cb9f4012c8f3487c0c460
Philip Burggraf <philip@pburggraf.de>
2017-03-30 17:47:33 +0200

    + german plates starting with t

---
+++ src/PB/LicensePlate/Detector/Germany/PlateTöl.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace PB\LicensePlate\Detector\Germany;
+
+use PB\LicensePlate\Detector\GermanyDetector;
+
+/**
+ * @author Philip Burggraf <philip@pburggraf.de>
+ */
+class PlateTöl extends AbstractGermanyPlate
+{
+    /**
+     * @var string[]
+     */
+    protected static $name = [
+        'Landkreis Bad Tölz-Wolfratshausen',
+    ];
+}
