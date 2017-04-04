257e6535488220a97caf71f87d9b6fee304a1351
Philip Burggraf <philip@pburggraf.de>
2017-04-04 19:22:23 +0200

    # completed specific tests starting with q and fixed bugs

---
+++ tests/PB/LicensePlate/Detector/Germany/PlateVibTest.php
@@ -0,0 +1,216 @@
+<?php
+
+namespace PB\LicensePlate\Detector\Germany;
+
+use PB\LicensePlate\Detector\GermanyDetector;
+use PB\LicensePlate\LicensePlateFactory;
+use PB\LicensePlate\Test\Detector\Germany\AbstractGermanyPlate;
+
+/**
+ * @author Philip Burggraf <philip@pburggraf.de>
+ */
+class PlateVibTest extends AbstractGermanyPlate
+{
+    /**
+     * @var array
+     */
+    protected static $plateTests = [
+        [
+            'plate' => 'VIB A 1',
+            'description' => [
+                'Landkreis Landshut'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'VIB A 10',
+            'description' => [
+                'Landkreis Landshut'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'VIB A 100',
+            'description' => [
+                'Landkreis Landshut'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'VIB A 1000',
+            'description' => [
+                'Landkreis Landshut'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+
+        [
+            'plate' => 'VIB B 1',
+            'description' => [
+                'Landkreis Landshut'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'VIB B 10',
+            'description' => [
+                'Landkreis Landshut'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'VIB B 100',
+            'description' => [
+                'Landkreis Rottal-Inn'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'VIB B 1000',
+            'description' => [
+                'Landkreis Landshut'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+
+        [
+            'plate' => 'VIB I 1',
+            'description' => [
+                'Landkreis Landshut'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'VIB I 10',
+            'description' => [
+                'Landkreis Landshut'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'VIB I 100',
+            'description' => [
+                'Landkreis Rottal-Inn'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'VIB I 1000',
+            'description' => [
+                'Landkreis Landshut'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+
+        [
+            'plate' => 'VIB O 1',
+            'description' => [
+                'Landkreis Landshut'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'VIB O 10',
+            'description' => [
+                'Landkreis Landshut'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'VIB O 100',
+            'description' => [
+                'Landkreis Rottal-Inn'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'VIB O 1000',
+            'description' => [
+                'Landkreis Landshut'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+
+        [
+            'plate' => 'VIB Q 1',
+            'description' => [
+                'Landkreis Landshut'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'VIB Q 10',
+            'description' => [
+                'Landkreis Landshut'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'VIB Q 100',
+            'description' => [
+                'Landkreis Rottal-Inn'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'VIB Q 1000',
+            'description' => [
+                'Landkreis Landshut'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+
+        [
+            'plate' => 'VIB AA 1',
+            'description' => [
+                'Landkreis Landshut'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'VIB AA 10',
+            'description' => [
+                'Landkreis Landshut'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'VIB AA 100',
+            'description' => [
+                'Landkreis Landshut'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'VIB AA 1000',
+            'description' => [
+                'Landkreis Landshut'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+    ];
+
+    /**
+     * @dataProvider getDataProvider
+     *
+     * @param $plate
+     * @param $description
+     * @param $type
+     */
+    public function testDescription($plate, $description, $type)
+    {
+        return parent::testDescription($plate, $description, $type);
+    }
+
+    /**
+     * @dataProvider getDataProvider
+     *
+     * @param $plate
+     * @param $description
+     * @param $type
+     */
+    public function testType($plate, $description, $type)
+     {
+         return parent::testType($plate, $description, $type);
+    }
+}
