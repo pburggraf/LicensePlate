4bc9a5c807b35f51158850281dcd4ce9ff623370
Philip Burggraf <philip@pburggraf.de>
2017-04-04 19:44:44 +0200

    # completed specific tests starting with z

---
+++ tests/PB/LicensePlate/Detector/Germany/PlateZwTest.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace PB\LicensePlate\Detector\Germany;
+
+use PB\LicensePlate\Detector\GermanyDetector;
+use PB\LicensePlate\LicensePlateFactory;
+use PB\LicensePlate\Test\Detector\Germany\AbstractGermanyPlate;
+
+/**
+ * @author Philip Burggraf <philip@pburggraf.de>
+ */
+class PlateZwTest extends AbstractGermanyPlate
+{
+    /**
+     * @var array
+     */
+    protected static $plateTests = [
+        [
+            'plate' => 'ZW A 1',
+            'description' => [
+                'Stadt Zweibrücken'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'ZW A 10',
+            'description' => [
+                'Stadt Zweibrücken'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'ZW A 100',
+            'description' => [
+                'Stadt Zweibrücken'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'ZW A 1000',
+            'description' => [
+                'Stadt Zweibrücken'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+
+        [
+            'plate' => 'ZW AA 1',
+            'description' => [
+                'Landkreis Südwestpfalz'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'ZW AA 10',
+            'description' => [
+                'Landkreis Südwestpfalz'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'ZW AA 100',
+            'description' => [
+                'Stadt Zweibrücken'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'ZW AA 1000',
+            'description' => [
+                'Stadt Zweibrücken'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+    ];
+
+    /**
+     * @dataProvider getDataProvider
+     *
+     * @param $plate
+     * @param $description
+     * @param $type
+     */
+    public function testDescription($plate, $description, $type)
+    {
+        return parent::testDescription($plate, $description, $type);
+    }
+
+    /**
+     * @dataProvider getDataProvider
+     *
+     * @param $plate
+     * @param $description
+     * @param $type
+     */
+    public function testType($plate, $description, $type)
+     {
+         return parent::testType($plate, $description, $type);
+    }
+}
