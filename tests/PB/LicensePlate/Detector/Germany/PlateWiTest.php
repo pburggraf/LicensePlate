b7c9b8abf85a35c25bf6364182924688cb0b45a0
Philip Burggraf <philip@pburggraf.de>
2017-04-04 19:41:27 +0200

    # completed specific tests starting with w and fixed bugs

---
+++ tests/PB/LicensePlate/Detector/Germany/PlateWiTest.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace PB\LicensePlate\Detector\Germany;
+
+use PB\LicensePlate\Detector\GermanyDetector;
+use PB\LicensePlate\LicensePlateFactory;
+use PB\LicensePlate\Test\Detector\Germany\AbstractGermanyPlate;
+
+/**
+ * @author Philip Burggraf <philip@pburggraf.de>
+ */
+class PlateWiTest extends AbstractGermanyPlate
+{
+    /**
+     * @var array
+     */
+    protected static $plateTests = [
+        [
+            'plate' => 'WI A 1',
+            'description' => [
+                'Stadt Wiesbaden'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'WI A 10',
+            'description' => [
+                'Stadt Wiesbaden'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'WI A 100',
+            'description' => [
+                'Stadt Wiesbaden'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'WI A 1000',
+            'description' => [
+                'Stadt Wiesbaden'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+
+        [
+            'plate' => 'WI AA 1',
+            'description' => [
+                'Stadt Wiesbaden'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'WI AA 10',
+            'description' => [
+                'Stadt Wiesbaden'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'WI AA 100',
+            'description' => [
+                'Stadt Wiesbaden'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'WI AA 1000',
+            'description' => [
+                'Stadt Wiesbaden'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'WI HP 1',
+            'description' => [
+                'Stadt Wiesbaden'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'WI HP 10',
+            'description' => [
+                'Stadt Wiesbaden'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_DEFAULT,
+        ],
+        [
+            'plate' => 'WI HP 100',
+            'description' => [
+                'Länderfahrzeug'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_LOCAL_POLICE,
+        ],
+        [
+            'plate' => 'WI HP 1000',
+            'description' => [
+                'Länderfahrzeug'
+            ],
+            'type' => GermanyDetector::PLATE_TYPE_LOCAL_POLICE,
+        ],
+    ];
+
+    /**
+     * @dataProvider getDataProvider
+     *
+     * @param $plate
+     * @param $description
+     * @param $type
+     */
+    public function testDescription($plate, $description, $type)
+    {
+        return parent::testDescription($plate, $description, $type);
+    }
+
+    /**
+     * @dataProvider getDataProvider
+     *
+     * @param $plate
+     * @param $description
+     * @param $type
+     */
+    public function testType($plate, $description, $type)
+     {
+         return parent::testType($plate, $description, $type);
+    }
+}
